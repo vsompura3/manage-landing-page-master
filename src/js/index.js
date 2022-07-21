2eb4b40e44c6849e3a9b1a540efdebb8e4a7fcff
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2022-07-22 01:01:34 +0530

    responsiveness and navigation menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
-console.log('💀')
+const primaryHeader = document.querySelector('.primary-header')
+const primaryNav = document.querySelector('.primary-navigation')
+const navToggle = document.querySelector('.mobile-nav-toggle')
+
+function handleClick() {
+  primaryNav.hasAttribute('data-visible')
+    ? navToggle.setAttribute('aria-expanded', false)
+    : navToggle.setAttribute('aria-expanded', true)
+  primaryNav.toggleAttribute('data-visible')
+  primaryHeader.toggleAttribute('data-overlay')
+}
+
+navToggle.addEventListener('click', handleClick)
